d1696459d0d955a1e2479b9da71756c437c788a8
IharTsykala <sudja09@bk.ru>
2024-10-01 11:59:11 +0500

    feat: create an iam role for github actions[task-1]

--- main.tf
+++ main.tf
@@ -6,3 +6,58 @@ terraform {
     encrypt        = true
   }
 }
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_iam_role" "GithubActionsRole" {
+  name               = "GithubActionsRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {
+      "Federated": "arn:aws:iam::aws:policy/OIDC"
+    },
+    "Action": "sts:AssumeRoleWithWebIdentity"
+  }
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "EC2FullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "Route53FullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "S3FullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "IAMFullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "VPCFullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "SQSFullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "EventBridgeFullAccess" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+}

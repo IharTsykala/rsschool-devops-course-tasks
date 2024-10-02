655283ef234a9e38265ba73e82f5a8c17bd8b295
IharTsykala <sudja09@bk.ru>
2024-10-02 10:07:53 +0500

    fix: formatted terraform code[task-1]

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-states-ihar-tsykala"
-    key            = "prod/terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
+    bucket  = "terraform-states-ihar-tsykala"
+    key     = "prod/terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
   }
 }
 
